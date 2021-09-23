bd775e61c074ccac24e6cb888f33115c6b217ca6
DeepakLenkadipun <deepaklenka318@gmail.com>
2021-09-23 11:19:35 +0530

    this is the camcastSeleniumfrm push to github

---
+++ ComcastSeleniumFrm/src/test/java/com/cast/ContactsTest/ContactTest.java
@@ -0,0 +1,26 @@
+package com.cast.ContactsTest;
+
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	
+	@Test(groups="smoketest")
+	public void createContactTest() {
+			
+			System.out.println("execute createcontactTest");
+		}
+	@Test(groups="regressiontest")
+	public void createContactWithOrgTest() {
+		
+		System.out.println("execute createcontactwithorgTest");
+	}
+	@Test(groups="regressiontest")
+	public void deletContactTest() {
+		
+		System.out.println("execute deletecontactTest");
+	}
+	
+
+}
